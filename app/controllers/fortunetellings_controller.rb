caa09eedaa38d79a7ff286518ba7d746b0111c23
N-Ryo <ryo.joy.joy@gmail.com>
2019-03-08 10:45:03 +0900

    Add URL-validation

--- app/controllers/fortunetellings_controller.rb
+++ app/controllers/fortunetellings_controller.rb
@@ -1,7 +1,19 @@
 class FortunetellingsController < ApplicationController
   # 計算用のモジュールCommon、検証用のモジュールFortunetellingsHelperをincludeする。
   include FortunetellingsHelper, Common
+  # before_actionでshowアクションが実行される前にset_dateメソッドを実行する。
+  before_action :set_date, only: [:show]
   def show
   end
 
+  private
+    def set_date
+      # FortunetellingsHelperのvalid_birthdayメソッドを実行して、有効な誕生日か判定する。
+      # 有効な場合は@dateを定義して、無効な場合は@error_messageを定義して、Viewに渡す。
+      if valid_birthday?(params[:date])
+        @date = params[:date]
+      else
+        @error_message = "半角英数字8桁であなたの誕生日を入力してください"
+      end
+    end
 end
--- app/helpers/fortunetellings_helper.rb
+++ app/helpers/fortunetellings_helper.rb
@@ -1,2 +1,15 @@
 module FortunetellingsHelper
+  # 入力されたparams[:date]が
+  #   8桁の半角数字・有効な日付・過去の日付
+  # になっているか判別する。
+  #有効でない場合はfalseを返す。
+  def valid_birthday?(date)
+    begin
+    date.length == 8 &&
+    Time.new(date[0,4], date[4,2], date[6,2]).strftime('%Y%m%d') <= Time.now.strftime('%Y%m%d')
+    rescue
+      false
+    end
+  end
+
 end
