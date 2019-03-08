d3cd8b7bf2b0c827e4ebef973d4875ce554de036
N-Ryo <ryo.joy.joy@gmail.com>
2019-03-08 10:43:52 +0900

    Add files to ready development

---
+++ app/controllers/fortunetellings_controller.rb
@@ -0,0 +1,7 @@
+class FortunetellingsController < ApplicationController
+  # 計算用のモジュールCommon、検証用のモジュールFortunetellingsHelperをincludeする。
+  include FortunetellingsHelper, Common
+  def show
+  end
+
+end
